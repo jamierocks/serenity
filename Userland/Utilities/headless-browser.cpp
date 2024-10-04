aefbd7485b464b2c23ddaefe66cb5234a4870835
Timothy Flynn <trflynn89@pm.me>
2024-10-04 11:10:40 -0400

    headless-browser: Define the test-related structures earlier
    And re-order the Test fields to have the test mode first.
    
    No functional change - this is just to reduce diff noise a bit in an
    upcoming patch.
    
    (cherry picked from commit 08426efebcb6457b0e8bf12c7ed9052f4460d8ba)

--- Userland/Utilities/headless-browser.cpp
+++ Userland/Utilities/headless-browser.cpp
@@ -60,6 +60,41 @@
 #    include <Ladybird/Utilities.h>
 #endif
 
+enum class TestMode {
+    Layout,
+    Text,
+    Ref,
+};
+
+enum class TestResult {
+    Pass,
+    Fail,
+    Skipped,
+    Timeout,
+};
+
+static constexpr StringView test_result_to_string(TestResult result)
+{
+    switch (result) {
+    case TestResult::Pass:
+        return "Pass"sv;
+    case TestResult::Fail:
+        return "Fail"sv;
+    case TestResult::Skipped:
+        return "Skipped"sv;
+    case TestResult::Timeout:
+        return "Timeout"sv;
+    }
+    VERIFY_NOT_REACHED();
+}
+
+struct Test {
+    TestMode mode;
+    ByteString input_path;
+    ByteString expectation_path;
+    Optional<TestResult> result;
+};
+
 class HeadlessWebContentView;
 
 struct Application {
@@ -320,34 +355,6 @@ static ErrorOr<NonnullRefPtr<Core::Timer>> load_page_for_screenshot_and_exit(Cor
     return timer;
 }
 
-enum class TestMode {
-    Layout,
-    Text,
-    Ref,
-};
-
-enum class TestResult {
-    Pass,
-    Fail,
-    Skipped,
-    Timeout,
-};
-
-static StringView test_result_to_string(TestResult result)
-{
-    switch (result) {
-    case TestResult::Pass:
-        return "Pass"sv;
-    case TestResult::Fail:
-        return "Fail"sv;
-    case TestResult::Skipped:
-        return "Skipped"sv;
-    case TestResult::Timeout:
-        return "Timeout"sv;
-    }
-    VERIFY_NOT_REACHED();
-}
-
 static ErrorOr<TestResult> run_dump_test(HeadlessWebContentView& view, URL::URL const& url, StringView expectation_path, TestMode mode, int timeout_in_milliseconds)
 {
     Core::EventLoop loop;
@@ -579,13 +586,6 @@ static ErrorOr<TestResult> run_test(HeadlessWebContentView& view, StringView inp
     }
 }
 
-struct Test {
-    ByteString input_path;
-    ByteString expectation_path;
-    TestMode mode;
-    Optional<TestResult> result;
-};
-
 static Vector<ByteString> s_skipped_tests;
 
 static ErrorOr<void> load_test_config(StringView test_root_path)
@@ -628,7 +628,7 @@ static ErrorOr<void> collect_dump_tests(Vector<Test>& tests, StringView path, St
         auto basename = LexicalPath::title(name);
         auto expectation_path = ByteString::formatted("{}/expected/{}/{}.txt", path, trail, basename);
 
-        tests.append({ input_path, move(expectation_path), mode, {} });
+        tests.append({ mode, input_path, move(expectation_path), {} });
     }
     return {};
 }
@@ -639,7 +639,7 @@ static ErrorOr<void> collect_ref_tests(Vector<Test>& tests, StringView path)
         if (entry.type == Core::DirectoryEntry::Type::Directory)
             return IterationDecision::Continue;
         auto input_path = TRY(FileSystem::real_path(ByteString::formatted("{}/{}", path, entry.name)));
-        tests.append({ input_path, {}, TestMode::Ref, {} });
+        tests.append({ TestMode::Ref, input_path, {}, {} });
         return IterationDecision::Continue;
     }));
 
